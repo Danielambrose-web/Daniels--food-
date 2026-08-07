22ee9cb00d0fe0065712347915c3948a0f19df8e
Danielambrose-web <danielessientech121@gmail.com>
2026-08-07 01:39:26 +0100

    mount it to app to test it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import './index.css';
 import Button from './components/Button/Button';
+import FoodCard from './components/FoodCard/FoodCard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to My App</h1>
-      <Button label="Primary Button" variant="primary" onClick={() => alert('Primary button clicked!')} />
-      <Button label="Secondary Button" variant="secondary" onClick={() => alert('Secondary button clicked!')} />
+      <FoodCard/>
+      {/* <Button label="Secondary Button" variant="secondary" onClick={() => alert('Secondary button clicked!')} /> */}
     </div>
   );
 }
