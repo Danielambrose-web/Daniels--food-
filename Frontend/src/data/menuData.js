98323386cd06962dc0453ae1009e510907c9f014
Danielambrose-web <danielessientech121@gmail.com>
2026-08-21 12:06:08 +0100

    updated imges to match the ones in public/images

--- Frontend/src/data/menuData.js
+++ Frontend/src/data/menuData.js
@@ -4,7 +4,7 @@ const menuData = [
     name: "Jollof Rice",
     price: 2500,
     description: "Smoky Nigerian jollof rice served with fried plantain.",
-    image: "/images/jollof-rice.jpg",
+    image: "/images/food_1.png",
     category: "Rice"
   },
 
@@ -13,7 +13,7 @@ const menuData = [
     name: "Fried Rice",
     price: 2800,
     description: "Flavorful fried rice packed with vegetables and spices.",
-    image: "/images/fried-rice.jpg",
+    image: "/images/food_2.png",
     category: "Rice"
   },
 
@@ -22,7 +22,7 @@ const menuData = [
     name: "Coconut Rice",
     price: 3000,
     description: "Creamy coconut-infused rice with a rich aromatic flavor.",
-    image: "/images/coconut-rice.jpg",
+    image: "/images/food_3.png",
     category: "Rice"
   },
 
@@ -31,7 +31,7 @@ const menuData = [
     name: "Grilled Chicken",
     price: 3500,
     description: "Juicy chicken grilled with our special house seasoning.",
-    image: "/images/grilled-chicken.jpg",
+    image: "/images/food_4.png",
     category: "Chicken"
   },
 
@@ -40,7 +40,7 @@ const menuData = [
     name: "Chicken Wings",
     price: 3000,
     description: "Crispy chicken wings tossed in a flavorful sauce.",
-    image: "/images/chicken-wings.jpg",
+    image: "/images/food_5.png",
     category: "Chicken"
   },
 
@@ -49,7 +49,7 @@ const menuData = [
     name: "Peppered Chicken",
     price: 4000,
     description: "Tender chicken cooked with spicy peppers and onions.",
-    image: "/images/peppered-chicken.jpg",
+    image: "/images/food_6.png",
     category: "Chicken"
   },
 
@@ -58,7 +58,7 @@ const menuData = [
     name: "Fried Plantain",
     price: 1500,
     description: "Sweet golden plantain fried until perfectly crispy.",
-    image: "/images/fried-plantain.jpg",
+    image: "/images/food_7.png",
     category: "Sides"
   },
 
@@ -67,7 +67,7 @@ const menuData = [
     name: "French Fries",
     price: 1800,
     description: "Crispy golden fries served hot and seasoned to perfection.",
-    image: "/images/french-fries.jpg",
+    image: "/images/food_8.png",
     category: "Sides"
   },
 
@@ -76,7 +76,7 @@ const menuData = [
     name: "Chapman",
     price: 2000,
     description: "Refreshing Nigerian-style cocktail with a fruity twist.",
-    image: "/images/chapman.jpg",
+    image: "/images/food_9.png",
     category: "Drinks"
   },
 
@@ -85,7 +85,7 @@ const menuData = [
     name: "Zobo",
     price: 1200,
     description: "Refreshing hibiscus drink infused with natural spices.",
-    image: "/images/zobo.jpg",
+    image: "/images/food_10.png",
     category: "Drinks"
   }
 ];
