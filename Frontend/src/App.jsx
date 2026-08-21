637a832d09d2ae84e485e0aac27d629f000a3d2a
Danielambrose-web <danielessientech121@gmail.com>
2026-08-21 11:32:37 +0100

    route the nav in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,27 @@
-// import { Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar/Navbar';
-// import Footer from './components/Footer/Footer';
-// import HomePage from './pages/Home/Home';
-// // Import your other pages here...
+import { Routes, Route } from "react-router-dom";
 
-// export default function App() {
-//   return (
-//     <div className="app-container">
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         {/* Add Menu, Cart, Checkout routes here as you build them */}
-//       </Routes>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-import React from "react";
-import MenuPage from "./pages/Menu/Menu";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+
+import HomePage from "./pages/Home/Home";
+import MenuPage from "./pages/Menu/Menu";
+// import CartPage from "./pages/Cart/Cart";
+// import CheckoutPage from "./pages/Checkout/Checkout";
+// import OrderConfirmationPage from "./pages/OderConfirmation/OrderConfirmation";
 
 export default function App() {
   return (
     <div className="app-container">
       <Navbar />
-      <MenuPage />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        {/* <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/order-confirmation" element={<OrderConfirmationPage />} /> */}
+      </Routes>
+
       <Footer />
     </div>
   );
