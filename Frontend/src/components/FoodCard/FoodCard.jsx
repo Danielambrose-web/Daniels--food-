26723a04d2f4bd87b50eba274191fbd0aa7d6814
Danielambrose-web <danielessientech121@gmail.com>
2026-08-22 21:43:42 +0100

    make the add to cart button work

--- Frontend/src/components/FoodCard/FoodCard.jsx
+++ Frontend/src/components/FoodCard/FoodCard.jsx
@@ -1,36 +1,47 @@
-import React from "react";
 import Button from "../Button/Button";
 import "./FoodCard.css";
 
-function handleAdd() {
-    alert("Item added!");
-}
-
-
-const FoodCard = ({ name, price, description, image, onAddToCart }) => {
+const FoodCard = ({
+  id,
+  name,
+  price,
+  description,
+  image,
+  onAddToCart,
+}) => {
   return (
-    <>
-      <div className="food-card">
-        <img
-        src={image} alt={name} 
-         className="food-card-image" />
-        <div className="food-card-info">
-          <div className="food-header">
-            <h3>{name}</h3>
-            <p className="food-price">{price}</p>
-          </div>
-          <p className="food-disc">
-            {description}
-          </p>
-          <Button
-            label="Add To Cart"
-            variant="primary"
-            onClick={() => alert(handleAdd(price,name))}
-            /*onClick={() => onAddToCart(name, price)*/
-          />
+    <div className="food-card">
+      <img
+        src={image}
+        alt={name}
+        className="food-card-image"
+      />
+
+      <div className="food-card-info">
+        <div className="food-header">
+          <h3>{name}</h3>
+          <p className="food-price">₦{price.toLocaleString()}</p>
         </div>
+
+        <p className="food-disc">
+          {description}
+        </p>
+
+        <Button
+          label="Add To Cart"
+          variant="primary"
+          onClick={() =>
+            onAddToCart({
+              id,
+              name,
+              price,
+              description,
+              image,
+            })
+          }
+        />
       </div>
-    </>
+    </div>
   );
 };
 
