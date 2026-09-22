b1ac28081b70741b82d5f486926650838a44e0aa
Danielambrose-webgit config --global user.email danielambrosetech121@gmsil.comgit config --global user.name Danielambrose-web <danielambrosetech121@gmsil.com>
2026-09-22 09:31:28 +0100

    add Contact page route and enhance responsiveness of Menu and FoodCard components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MenuPage from "./pages/Menu/Menu";
 import CartPage from "./pages/Cart/Cart";
 import Checkout from "./pages/Checkout/Checkout";
 import AboutPage from "./pages/About/About";
+import ContactPage from "./pages/Contact/Contact";
 // import OrderConfirmationPage from "./pages/OderConfirmation/OrderConfirmation";
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<Checkout/>} />
         {/*<Route path="/order-confirmation" element={<OrderConfirmationPage />} /> */}
